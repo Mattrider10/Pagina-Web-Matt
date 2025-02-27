58c5972abf99a70a061b8a84d7f889e1e60ea6d5
Mattrider10 <147452851+Mattrider10@users.noreply.github.com>
2025-02-27 17:53:06 -0600

    Add files via upload

---
+++ Pagina Web Matt/Main.js
@@ -0,0 +1,20 @@
+function fotos(){
+    window.location.replace("Fotos.html");
+}
+
+function tienda(){
+    window.location.replace("Tienda.html");
+}
+
+function contacto(){
+    window.location.replace("Contacto.html");
+}
+
+function eventos(){
+    window.location.replace("Eventos.html");
+}
+
+function inicio(){
+    window.location.replace("Index.html");
+}
+
